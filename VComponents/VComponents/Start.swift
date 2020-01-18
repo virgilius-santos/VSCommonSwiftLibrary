f08d4ef2eec4b08aea242ccbebaf36d02d7d3950
virgilius <virgilius.santos@acad.pucrs.br>
2020-01-18 07:52:20 -0300

    update workspace

---
+++ VComponents/VComponents/Start.swift
@@ -0,0 +1,13 @@
+//
+//  Start.swift
+//  VComponents
+//
+//  Created by Virgilius Santos on 18/01/20.
+//  Copyright © 2020 Virgilius Santos. All rights reserved.
+//
+
+import Foundation
+
+struct Start {
+    
+}
