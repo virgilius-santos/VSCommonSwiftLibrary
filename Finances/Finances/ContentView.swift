ea0f8a968d939b2ffa88be441fc3e6e509e72ca9
Virgilius <virgilius_santos@yahoo.com.br>
2021-04-10 13:54:01 -0300

    add finances project

---
+++ Finances/Finances/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finances
+//
+//  Created by Virgilius Santos on 10/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finances/Finances/FinancesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancesApp.swift
+//  Finances
+//
+//  Created by Virgilius Santos on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FinancesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
