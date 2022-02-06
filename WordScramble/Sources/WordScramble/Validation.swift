1536758f8ba3d0b576d13de3eac43c50f39c18ef
Virgilius <virgilius_santos@yahoo.com.br>
2022-02-06 15:47:21 -0300

    update

---
+++ WordScramble/Sources/WordScramble/Validation.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+enum Validation {
+    case success
+    case error(message: AlertMessage)
+}
+
+typealias ValidationFunction = (_ word: String, _ usedWords: [String], _ rootWord: String) -> Validation
+
+extension Array where Element == ValidationFunction {
+    func validate(_ word: String, _ usedWords: [String], _ rootWord: String) -> Validation {
+        for rule in self {
+            let result = rule(word, usedWords, rootWord)
+            switch result {
+            case .success:
+                continue
+            case .error:
+                return result
+            }
+        }
+        return .success
+    }
+}
