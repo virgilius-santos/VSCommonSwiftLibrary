f14e86c792ddc80c46be9c2a8616a57ddf380a95
Virgilius <virgilius_santos@yahoo.com.br>
2022-02-12 11:58:44 -0300

    moved SearchText to common lib

--- VSLibrary/Sources/Styles/CommonUI/SearchText.swift
+++ VSLibrary/Sources/Styles/CommonUI/SearchText.swift
@@ -1,25 +1,30 @@
 import SwiftUI
 
-struct SearchText: View {
+public struct SearchText: View {
+  public let title: String
   @Binding var searchText: String
   
-  var body: some View {
+  public var body: some View {
     HStack {
       
       Image(systemName: "magnifyingglass")
         .foregroundColor(Color.gray)
       
-      TextField("Search", text: $searchText)
+      TextField(title, text: $searchText)
         .textFieldStyle(.plain)
     }
     .padding(.vertical, 8)
     .padding(.horizontal)
     .background(Color.primary.opacity(0.15))
-    .cornerRadius(10)
+    .cornerRadius(.radius_10)
     .padding()
   }
   
-  init(searchText: Binding<String>) {
+  public init(
+    title: String = "Search",
+    searchText: Binding<String>
+  ) {
+    self.title = title
     _searchText = searchText
   }
 }
